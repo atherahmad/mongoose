705cf388f1b16c0092af573da650b870fda551f7
Ather Ahmad <atherahmad49@yahoo.com>
2022-07-04 09:48:09 +0200

    data base added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const config = require("config");
+const mongoose = require("mongoose");
 
+mongoose.connect(config.get('db.uri'),(err)=>console.log(`Mongo Atlas Connected`))
 
 const PORT = config.get('app.port') || 4000;
 const app = express();
